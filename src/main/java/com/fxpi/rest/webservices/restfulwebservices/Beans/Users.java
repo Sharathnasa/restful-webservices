1d3a95f7d4dce3e6c359495054ae9a060e6b74b3
sharath <sharathnasa@gmail.com>
2020-05-04 10:01:21 +0530

    1. Integrated Swagger. 2. Also added the description about the field to swagger using ApiModel annotations

--- src/main/java/com/fxpi/rest/webservices/restfulwebservices/Beans/Users.java
+++ src/main/java/com/fxpi/rest/webservices/restfulwebservices/Beans/Users.java
@@ -1,17 +1,23 @@
 package com.fxpi.rest.webservices.restfulwebservices.Beans;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user.")
 public class Users {
 
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast two characters")
+    @ApiModelProperty(notes = "Name should have atleast two characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date should be in past")
     private Date birthDate;
 
     public Users(Integer id, String name, Date birthDate) {
--- src/main/java/com/fxpi/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/fxpi/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -4,9 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
